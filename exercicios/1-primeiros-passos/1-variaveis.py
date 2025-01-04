1901a7935e1a15f0b9fba757f195402a7a90396d
Brunogsw07 <brunogswagner@gmail.com>
2025-01-04 23:57:18 +0000

    Resolução

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,13 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Bruno'
+seg_nome = 'Guilherme'
 
 # Declare variáveis de cada tipo numérico
-
+idade = 29
 
 # Declare uma variável do tipo booleano
+altura = 1.89
+
+print(nome, seg_nome, idade, altura)
