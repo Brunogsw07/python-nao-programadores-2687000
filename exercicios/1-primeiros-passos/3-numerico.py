6a5a44d7cb7f7ba8660562361b9c27c61f30299e
Brunogsw07 <brunogswagner@gmail.com>
2025-01-07 00:18:29 +0000

    Resolução

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,16 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
+data_nascimento*idade_numerica
 
 
 # Realize uma operação entre dados do tipo inteiro e float
+teste = idade_numerica*altura
+print(teste)
+round(teste,1)
+round(teste,0)
--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -2,9 +2,27 @@ ano_nascimento = 1989
 ano_formatura = 2010
 
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
-
+idade = ano_formatura-ano_nascimento
+print(idade)
 
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
-
+ano_nascimento > ano_formatura
+ano_nascimento < ano_formatura
+2 == 2
+ano_formatura == ano_nascimento
+not ano_nascimento == ano_formatura
+'bruno' != 'teste'
 
 # Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+
+ano_nascimento == 1989 and ano_formatura == 2010
+a = 10
+b = 20
+c = 30
+
+a + b == 30
+a or b == 20
+a == 20 or b == 20
+a == 20 and b == 20
+not a == 20
+not b == 20
